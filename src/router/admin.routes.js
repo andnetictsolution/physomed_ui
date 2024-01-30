8aeb80273c86af5e4edd9b5f5fddbb9148faab9d
KHA <kiday212@gmail.com>
2024-01-30 11:17:57 +0300

    patient registartion

--- src/router/admin.routes.js
+++ src/router/admin.routes.js
@@ -3,13 +3,19 @@ const adminRoutes = [
     path: '/admin/dashboard',
     name: 'AdminDashboard',
     component: () => import('../views/admin/AdminDashboard.vue'),
-    meta: { requiresAuth: false, userGuard: 'All',layout:"default" }
+    meta: { requiresAuth: false, userGuard: 'All', layout: "default" }
   },
   {
     path: '/user/register',
     name: 'userRegister',
     component: () => import('../views/admin/UserRegistration.vue'),
-    meta: { requiresAuth: false, userGuard: 'All',layout:"auth" }
+    meta: { requiresAuth: false, userGuard: 'All', layout: "auth" }
+  },
+  {
+    path: '/patient/register',
+    name: 'patientRegister',
+    component: () => import('../views/admin/PatientRegistration.vue'),
+    meta: { requiresAuth: false, userGuard: 'All', layout: "auth" }
   }
 ]
 
