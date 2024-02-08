eab86baedc53ccf2465b5fb67417285a408bc7dc
Flah <haftomg96@gmail.com>
2024-02-08 23:59:41 +0300

    logout function implemented

--- src/components/auth/authHeader.vue
+++ src/components/auth/authHeader.vue
@@ -16,8 +16,8 @@
           data-dropdown-toggle="user-dropdown"
           data-dropdown-placement="bottom"
         >
-          <span class="sr-only">Open user menu</span
-          ><img class="w-8 h-8 rounded-full" src="../../assets/andnet.jpeg" alt="user photo" />
+          <!-- <span class="sr-only">Open user menu</span> -->
+          <img class="w-8 h-8 rounded-full" src="../../assets/andnet.jpeg" alt="user photo" />
         </button>
         <!-- Dropdown menu -->
         <div
@@ -25,124 +25,53 @@
           id="user-dropdown"
         >
           <div class="px-4 py-3">
-            <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-            <span class="block text-sm text-gray-500 truncate dark:text-gray-400"
-              >name@flowbite.com</span
-            >
+            <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ user }}</span>
+            <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ role }}</span>
           </div>
           <ul class="py-2" aria-labelledby="user-menu-button">
             <li>
               <a
-                href="#"
                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >Dashboard</a
+                >Edit Profile</a
               >
             </li>
             <li>
               <a
-                href="#"
                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >Settings</a
+                >Change Password</a
               >
             </li>
             <li>
               <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >Earnings</a
-              >
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >Sign out</a
+                @click="logout"
+                class="block px-4 pointer py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >logout</a
               >
             </li>
           </ul>
         </div>
-        <button
-          data-collapse-toggle="navbar-user"
-          type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-user"
-          aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-      </div>
-      <div
-        class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-        id="navbar-user"
-      >
-        <ul
-          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
-        >
-          <li>
-            <a
-              href="/"
-              class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-              aria-current="page"
-              >Home</a
-            >
-          </li>
-          <li>
-            <a
-              href="/about"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >About
-            </a>
-          </li>
-          <li>
-            <a
-              href="/service"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >Services</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >Pricing</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >Contact</a
-            >
-          </li>
-        </ul>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import { reactive, ref, onMounted, onUnmounted } from 'vue'
-
+import { authStore } from '@/stores/auth/auth'
+import { reactive, ref, onMounted, onUnmounted, computed } from 'vue'
 export default {
   setup() {
     const windowWidth = ref(window.innerWidth)
+    const authPinia = authStore()
     const isMobileMenuOpen = reactive(false)
-
+    const user = computed(() => {
+      return localStorage.getItem('physomed_user')
+    })
+    const role = computed(() => {
+      return localStorage.getItem('physomed_role')
+    })
+    const logout = () => {
+      authPinia.logoutUser()
+    }
     const toggleMobileMenu = () => {
       isMobileMenuOpen.value = !isMobileMenuOpen.value
     }
@@ -158,6 +87,7 @@ export default {
 
     onMounted(() => {
       window.addEventListener('resize', handleResize)
+     
     })
 
     onUnmounted(() => {
@@ -174,7 +104,10 @@ export default {
       isMobileMenuOpen,
       toggleMobileMenu,
       getNavClass,
-      isMobile
+      isMobile,
+      user,
+      role,
+      logout
     }
   }
 }
