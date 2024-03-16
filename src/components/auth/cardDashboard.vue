d8b13f30c65b93b03e08eec281250022479b228c
mosalih7 <60650945+mosalih7@users.noreply.github.com>
2024-03-16 08:14:36 +0300

    fix dashboard labels

--- src/components/auth/cardDashboard.vue
+++ src/components/auth/cardDashboard.vue
@@ -1,36 +1,39 @@
 <template>
-        <a href="#" class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-            <p class="me-3 w-7 h-7" >
-                {{ number }}
-            </p>
-            <div class="text-left rtl:text-right">
-                <div class="mb-1 text-xs">{{ description }}</div>
-                <div class="-mt-1 font-sans text-sm font-semibold">{{ title }}</div>
-            </div>
-        </a>
+  <a
+    href="#"
+    class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+  >
+    <p class="me-3 w-7 h-7">
+      {{ props.number }}
+    </p>
+    <div class="text-left rtl:text-right">
+      <div class="mb-1 text-xs">{{ props.description }}</div>
+      <!-- <div class="-mt-1 font-sans text-sm font-semibold">{{ title }}</div> -->
+    </div>
+  </a>
 </template>
 <script setup>
 const props = defineProps({
-    number: String,
-    title: String,
-    description: String,
+  number: String,
+  title: String,
+  description: String
 })
 </script>
 <style>
 .card:last-child {
-    margin-bottom: 0;
+  margin-bottom: 0;
 }
 
 .mb-0 {
-    margin-bottom: 0rem !important;
+  margin-bottom: 0rem !important;
 }
 
 .card {
-    background: var(--surface-card);
-    border: 1px solid var(--surface-border);
-    padding: 2rem;
-    margin-bottom: 2rem;
-    box-shadow: var(--card-shadow);
-    border-radius: 12px;
+  background: var(--surface-card);
+  border: 1px solid var(--surface-border);
+  padding: 2rem;
+  margin-bottom: 2rem;
+  box-shadow: var(--card-shadow);
+  border-radius: 12px;
 }
 </style>
--- src/views/admin/AdminDashboard.vue
+++ src/views/admin/AdminDashboard.vue
@@ -1,43 +1,50 @@
 <script setup>
-import cardDashboard from '@/components/auth/cardDashboard.vue';
-import Toolbar from 'primevue/toolbar';
-import Dropdown from 'primevue/dropdown';
-import MultiSelect from 'primevue/multiselect';
-import { reportStore } from '@/stores/admin/report';
-import { onMounted, ref, computed } from "vue"
-import { authStore } from '@/stores/auth/auth';
-import { serviceStore } from '@/stores/admin/service';
-import Toast from 'primevue/toast';
-import { useToast } from 'primevue/usetoast';
-import { convertToString } from '@/utils/moment';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import { userStore } from '@/stores/admin/user';
-import Dialog from 'primevue/dialog';
-const reportPinia = reportStore();
-const userPinia = userStore();
-const authPinia = authStore();
+import cardDashboard from '@/components/auth/cardDashboard.vue'
+import Toolbar from 'primevue/toolbar'
+import Dropdown from 'primevue/dropdown'
+import MultiSelect from 'primevue/multiselect'
+import { reportStore } from '@/stores/admin/report'
+import { onMounted, ref, computed } from 'vue'
+import { authStore } from '@/stores/auth/auth'
+import { serviceStore } from '@/stores/admin/service'
+import Toast from 'primevue/toast'
+import { useToast } from 'primevue/usetoast'
+import { convertToString } from '@/utils/moment'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import { userStore } from '@/stores/admin/user'
+import Dialog from 'primevue/dialog'
+import { patientStore } from '@/stores/reception/patient'
+const reportPinia = reportStore()
+const userPinia = userStore()
+const authPinia = authStore()
 const servicePinia = serviceStore()
 const toast = useToast()
+const patientPinia = patientStore()
 onMounted(async () => {
-  authPinia.setTitle("Report Analytics")
+  authPinia.setTitle('Report Analytics')
   await reportPinia.fetchRevenue({
     start_date: new Date(),
     end_date: new Date()
-  });
+  })
   await reportPinia.fetchCompareRevenue({
     start_date: new Date(),
     end_date: new Date(),
-    interval: "Monthly"
+    interval: 'Monthly'
   })
   await userPinia.fetchUsers()
   await servicePinia.fetchServices()
+  await patientPinia.fetchPatients()
 })
-const physioId = ref('')
-const openModal = ((id) => {
+
+const allPatients = computed(() => {
+  return patientPinia.getAllPatients
+})
+
+const openModal = (id) => {
   phsioDate.value.id = id
   visible.value = true
-})
+}
 const allServices = computed(() => {
   return servicePinia.getAllServices
 })
@@ -50,7 +57,7 @@ const allCompareRevenue = computed(() => {
 const physiotherapist = computed(() => {
   return userPinia.getAllUsers
 })
-const visible = ref(false);
+const visible = ref(false)
 const phsioDate = ref({
   start_date: '',
   end_date: '',
@@ -59,14 +66,13 @@ const phsioDate = ref({
 
 const intervals = [
   {
-    name: "Monthly",
-    value: "Monthly"
+    name: 'Monthly',
+    value: 'Monthly'
   },
   {
-    name: "Weekly",
-    value: "Weekly"
-  },
-
+    name: 'Weekly',
+    value: 'Weekly'
+  }
 ]
 const report = ref({
   start_date: '',
@@ -83,21 +89,28 @@ const allPhysio = computed(() => {
 const filterReport = async () => {
   try {
     await reportPinia.fetchRevenue({
-      start_date: new Date(report.start_date),
-      end_date: new Date(report.end_date)
+      start_date: new Date(report.value.start_date),
+      end_date: new Date(report.value.end_date)
     })
   } catch (error) {
     console.log(error)
   }
 }
+const filteredRoles = computed(() => {
+  return physiotherapist.value.filter((item) => item.role.name == 'Physiotherapist')
+})
 const filterCompareReport = async () => {
   try {
-    await reportPinia.fetchCompareRevenue(report.value);
-    toast.add({ severity: "success", summary: "Message", detail: "Report Updated", life: 6000 })
+    await reportPinia.fetchCompareRevenue(report.value)
+    toast.add({ severity: 'success', summary: 'Message', detail: 'Report Updated', life: 6000 })
   } catch (error) {
-    toast.add({ severity: "error", summary: "Message", detail: error.response.data.message, life: 6000 })
+    toast.add({
+      severity: 'error',
+      summary: 'Message',
+      detail: error.response.data.message,
+      life: 6000
+    })
   }
-
 }
 </script>
 <template>
@@ -107,57 +120,94 @@ const filterCompareReport = async () => {
       <template #start>
         <div class="flex flex-col md:flex-row">
           <div class="md:mr-2 w-96 md:w-full">
-            <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white">Start Date:</label>
-            <input v-model="report.start_date" id="name" type="date"
+            <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white"
+              >Start Date:</label
+            >
+            <input
+              v-model="report.start_date"
+              id="name"
+              type="date"
               class="w-full px-3 py-2 dark:text-black border border-gray-300 rounded focus:outline-none focus:border-primary"
-              placeholder="" />
+              placeholder=""
+            />
           </div>
-
+          {{ filteredRoles }}
           <div class="">
-            <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white">End Date:</label>
-            <input v-model="report.end_date" id="name" type="date"
+            <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white"
+              >End Date:</label
+            >
+            <input
+              v-model="report.end_date"
+              id="name"
+              type="date"
               class="w-full px-3 py-2 dark:text-black border border-gray-300 rounded focus:outline-none focus:border-primary"
-              @change="filterReport" placeholder="" />
+              @change="filterReport"
+              placeholder=""
+            />
           </div>
         </div>
-
-
       </template>
 
       <template #center>
-        <div class=" flex-col md:flex-row md:w-full">
+        <div class="flex-col md:flex-row md:w-full">
           <div class="flex flex-col md:flex-row">
-            <div class="mr-2 ">
-              <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white">Interval:</label>
-              <Dropdown v-model="report.interval" :options="intervals" optionValue="name" optionLabel="name"
-                placeholder="Select interval" class="w-full md:w-14rem" />
+            <div class="mr-2">
+              <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white"
+                >Interval:</label
+              >
+              <Dropdown
+                v-model="report.interval"
+                :options="intervals"
+                optionValue="name"
+                optionLabel="name"
+                placeholder="Select interval"
+                class="w-full md:w-14rem"
+              />
             </div>
 
             <div class="w-96 md:w-full">
-              <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white">Service list:</label>
-              <MultiSelect v-model="report.service_list" display="chip" :options="allServices" optionValue="_id"
-                optionLabel="name" placeholder="Select Cities" :maxSelectedLabels="3" class="w-full md:w-[20rem]" />
+              <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white"
+                >Service list:</label
+              >
+              <MultiSelect
+                v-model="report.service_list"
+                display="chip"
+                :options="allServices"
+                optionValue="_id"
+                optionLabel="name"
+                placeholder="Select Services"
+                :maxSelectedLabels="3"
+                class="w-full md:w-[20rem]"
+              />
             </div>
           </div>
         </div>
       </template>
 
       <template #end>
-        <button class="bg-primary rounded text-white p-2 md:mt-8 align-items-center" @click="filterCompareReport">
+        <button
+          class="bg-primary rounded text-white p-2 md:mt-8 align-items-center"
+          @click="filterCompareReport"
+        >
           Apply Filter
         </button>
       </template>
     </Toolbar>
-    <h3 class="font-bold text-xl my-1 ">Number Statics</h3>
+    <h3 class="font-bold text-xl my-1">Number Statics</h3>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
-      <cardDashboard description="Revenue" :number="allRevenue.revenue" title="here" />
-      <cardDashboard description="Revenue" number="12" title="here" />
-      <cardDashboard description="Revenue" number="12" title="here" />
+      <cardDashboard description="Revenue" :number="allRevenue.revenue" />
+      <cardDashboard description="Patients" :number="allPatients.length" />
+      <cardDashboard description="Revenue" :number="physiotherapist.length" />
     </div>
-    <h3 class="font-bold text-xl my-1 ">Revenue Report</h3>
+    <h3 class="font-bold text-xl my-1">Revenue Report</h3>
 
-    <DataTable class="h-screen" :value="[allRevenue]" scrollable scrollHeight="600px" tableStyle="min-width: 50rem">
-
+    <DataTable
+      class="h-screen"
+      :value="[allRevenue]"
+      scrollable
+      scrollHeight="600px"
+      tableStyle="min-width: 50rem"
+    >
       <Column header="Start date">
         <template #body="slotProps">
           <p>{{ convertToString(slotProps.data.start_date) }}</p>
@@ -173,13 +223,15 @@ const filterCompareReport = async () => {
           <p>{{ slotProps.data.revenue + ' Birr' }}</p>
         </template>
       </Column>
-
-
     </DataTable>
-    <h3 class="font-bold text-xl my-1 ">Revenue Compare</h3>
-    <DataTable class="h-screen" :value="allCompareRevenue.revenue_data" scrollable scrollHeight="600px"
-      tableStyle="min-width: 50rem">
-
+    <h3 class="font-bold text-xl my-1">Revenue Compare</h3>
+    <DataTable
+      class="h-screen"
+      :value="allCompareRevenue.revenue_data"
+      scrollable
+      scrollHeight="600px"
+      tableStyle="min-width: 50rem"
+    >
       <Column header="Start date">
         <template #body="slotProps">
           <p>{{ convertToString(slotProps.data.startDate) }}</p>
@@ -205,18 +257,21 @@ const filterCompareReport = async () => {
           <p>{{ slotProps.data.revenue + ' Birr' }}</p>
         </template>
       </Column>
-
-
     </DataTable>
-    <h3 class="font-bold text-xl my-1 ">Physiotherapist work statistics</h3>
-    <DataTable class="h-screen" :value="physiotherapist" scrollable scrollHeight="600px" tableStyle="min-width: 50rem">
-
-      <Column header="Start date">
+    <h3 class="font-bold text-xl my-1">Physiotherapist work statistics</h3>
+    <DataTable
+      class="h-screen"
+      :value="physiotherapist"
+      scrollable
+      scrollHeight="600px"
+      tableStyle="min-width: 50rem"
+    >
+      <Column header="Full Name">
         <template #body="slotProps">
-          <p class="capitalize">{{ slotProps.data.first_name + " " + slotProps.data.last_name }}</p>
+          <p class="capitalize">{{ slotProps.data.first_name + ' ' + slotProps.data.last_name }}</p>
         </template>
       </Column>
-      <Column header="End date">
+      <Column header="Role">
         <template #body="slotProps">
           <p>{{ slotProps.data.role.name }}</p>
         </template>
@@ -228,33 +283,48 @@ const filterCompareReport = async () => {
           </button>
         </template>
       </Column>
-
-
-
     </DataTable>
     <Dialog v-model:visible="visible" modal header="Work statics" :style="{ width: '25rem' }">
       <div class="flex flex-col md:flex-row">
         <div class="md:mr-2 w-96 md:w-full">
-          <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white">Start Date:</label>
-          <input v-model="phsioDate.start_date" id="name" type="date"
+          <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white"
+            >Start Date:</label
+          >
+          <input
+            v-model="phsioDate.start_date"
+            id="name"
+            type="date"
             class="w-full px-3 py-2 dark:text-black border border-gray-300 rounded focus:outline-none focus:border-primary"
-            placeholder="" />
+            placeholder=""
+          />
         </div>
-
 
         <div class="">
-          <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white">End Date:</label>
-          <input v-model="phsioDate.end_date" id="name" type="date"
+          <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white"
+            >End Date:</label
+          >
+          <input
+            v-model="phsioDate.end_date"
+            id="name"
+            type="date"
             class="w-full px-3 py-2 dark:text-black border border-gray-300 rounded focus:outline-none focus:border-primary"
-            @change="filterPhysioReport" placeholder="" />
+            @change="filterPhysioReport"
+            placeholder=""
+          />
         </div>
       </div>
-      <DataTable class="h-screen" :value="allPhysio" scrollable scrollHeight="600px"
-        tableStyle="min-width: 50rem">
-
+      <DataTable
+        class="h-screen"
+        :value="allPhysio"
+        scrollable
+        scrollHeight="600px"
+        tableStyle="min-width: 50rem"
+      >
         <Column header="Patient">
           <template #body="slotProps">
-            <p class="capitalize">{{ slotProps.data.patient.first_name + " " + slotProps.data.patient.last_name }}</p>
+            <p class="capitalize">
+              {{ slotProps.data.patient.first_name + ' ' + slotProps.data.patient.last_name }}
+            </p>
           </template>
         </Column>
         <Column header="Treatment">
@@ -262,22 +332,22 @@ const filterCompareReport = async () => {
             <p>{{ slotProps.data.service.name }}</p>
           </template>
         </Column>
-        <Column header="Action">
-          
-        </Column>
-
       </DataTable>
       <div class="flex justify-center gap-3 my-2">
-        <Button class="bg-gray-200 hover:bg-primary text-black hover:text-white font-bold py-2 px-4 rounded"
-          @click="visible = false">
+        <Button
+          class="bg-gray-200 hover:bg-primary text-black hover:text-white font-bold py-2 px-4 rounded"
+          @click="visible = false"
+        >
           Cancel
         </Button>
-        <Button @click="visible = false" class="bg-primary hover:bg-primary text-white font-bold py-2 px-4 rounded">
+        <Button
+          @click="visible = false"
+          class="bg-primary hover:bg-primary text-white font-bold py-2 px-4 rounded"
+        >
           ok
         </Button>
       </div>
     </Dialog>
-
   </div>
 </template>
 
